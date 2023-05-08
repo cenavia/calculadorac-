21ffd82716a87f3c95ccd909b662fb2cc800a145
Carlos Navia <carlos.navia@externos.bci.cl>
2023-05-08 14:04:15 -0500

    Creacion Menu calculadora

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Calculadora Científica");
+            Console.WriteLine("----------------------");
+
+            Console.Write("Ingrese el primer número: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo número: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese la operación (+, -, *, /, ^, sqrt): ");
+            string op = Console.ReadLine();
         }
     }
 }
